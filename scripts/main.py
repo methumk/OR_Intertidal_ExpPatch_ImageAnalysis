e6221998fe10a05ff48abbef601080ae99f10c27
Kyle Huang <huangkyle110@gmail.com>
2022-05-16 16:35:54 -0700

    updated tiled, end of term
    As much as we worked on for ResNet50 before switching to YOLOv5

--- scripts/main.py
+++ scripts/main.py
@@ -22,9 +22,10 @@ logger = logging.getLogger("main.py")
 
 img_transform = transforms.ToPILImage()
 
-lr = 0.01
+lr = 0.001
 batch_size = 1
-GENERATE_BBOX = False
+BATCH_SIZE = 128
+GENERATE_BBOX = True
 SHOW_IMG = True
 
 
@@ -35,6 +36,7 @@ def device_selector(use_cuda=True):
     Returns: (torch.device) Device the training is going to be run on.
     """
     if not use_cuda:
+        logger.info("Using Device: [ CPU ]")
         return "cpu"
     use_cuda = torch.cuda.is_available()
     if use_cuda:
@@ -65,7 +67,7 @@ class Bbox:
         self.im.save(fname)
 
 
-def windowed_collate_fn(batch):
+def windowed_collate_fn3(batch):
     w, h = 64, 64
     # Batch: (image, dict of labels)
     # Get num of labels in a batch to preallocate tensor space
@@ -100,7 +102,7 @@ def windowed_collate_fn(batch):
                     im1 = img_transform(windowed_img)
                     #im1.show()
                 windowed_c, windowed_h, windowed_w = windowed_img.shape
-                pad = (((2 * h - windowed_h) // 2), ((2 * h - windowed_h + 1) // 2), ((2 * w - windowed_w) // 2), ((2 * w - windowed_w + 1) // 2))
+                # pad = (((2 * h - windowed_h) // 2), ((2 * h - windowed_h + 1) // 2), ((2 * w - windowed_w) // 2), ((2 * w - windowed_w + 1) // 2))
                 pad = (((2 * w - windowed_w) // 2), ((2 * w - windowed_w+1) // 2), ((2 * h - windowed_h) // 2),
                        ((2 * h - windowed_h+1) // 2))
                 padded_img = F.pad(
@@ -108,8 +110,8 @@ def windowed_collate_fn(batch):
                     pad=pad,
                     mode='constant',
                 )
-                im = img_transform(padded_img)
-                #im.show()
+                # im = img_transform(padded_img)
+                # im.show()
 
                 # Verify that shape of the padded image is the expected shape.
                 assert padded_img.shape == (3, 2 * w, 2 * h), (padded_img.shape, (3, 2 * w, 2 * h), i)
@@ -123,8 +125,72 @@ def windowed_collate_fn(batch):
     return windowed_imgs, windowed_labels
 
 
+def windowed_collate_fn(batch):
+    w, h = 64, 64
+    # w, h = 224, 224
+    # Batch: (image, dict of labels)
+    # Get num of labels in a batch to preallocate tensor space
+    n_labels = sum([len(k) for k in batch[0][1].values()])
+    windowed_imgs, windowed_labels = torch.zeros((BATCH_SIZE, 3, 2 * w, 2 * h)), torch.zeros(BATCH_SIZE)
+    all_imgs, all_labels = [], []
+    i = 0
+    for data, labels in batch:
+        # Add bboxes around all labels present in image
+        if GENERATE_BBOX:
+            bbox_im = Bbox(data)
+
+        C, Y_max, X_max = data.shape
+        for label, center_pt in labels.items():
+            # Skip empty label
+            if not center_pt:
+                continue
+            j = 0
+            for x, y in center_pt:
+                assert x <= X_max
+                assert y <= Y_max
+
+                # Padding bounds need to account for edge cases.
+                w_lower_pad, w_upper_pad = max(0, x - w), min(X_max, x + w)
+                h_lower_pad, h_upper_pad = max(0, y - h), min(Y_max, y + h)
+
+                if GENERATE_BBOX:
+                    bbox_im.add_bbox(w_lower_pad, h_lower_pad, w_upper_pad, h_upper_pad, label)
+
+                # windowed_img = data[:, w_lower_pad:w_upper_pad, h_lower_pad:h_upper_pad]
+                windowed_img = data[:, h_lower_pad:h_upper_pad, w_lower_pad:w_upper_pad]
+                if windowed_img.nelement() != 0:
+                    im1 = img_transform(windowed_img)
+                    #im1.show()
+                windowed_c, windowed_h, windowed_w = windowed_img.shape
+                # pad = (((2 * h - windowed_h) // 2), ((2 * h - windowed_h + 1) // 2), ((2 * w - windowed_w) // 2), ((2 * w - windowed_w + 1) // 2))
+                pad = (((2 * w - windowed_w) // 2), ((2 * w - windowed_w+1) // 2), ((2 * h - windowed_h) // 2),
+                       ((2 * h - windowed_h+1) // 2))
+                padded_img = F.pad(
+                    input=windowed_img,
+                    pad=pad,
+                    mode='constant',
+                )
+                # im = img_transform(padded_img)
+                # im.show()
+
+                # Verify that shape of the padded image is the expected shape.
+                assert padded_img.shape == (3, 2 * w, 2 * h), (padded_img.shape, (3, 2 * w, 2 * h), i)
+                windowed_imgs[j] = padded_img
+                windowed_labels[j] = label
+                if j == 7:
+                    all_imgs.append(windowed_imgs)
+                    all_labels.append(windowed_labels)
+                i += 1
+                j = i % 8
+
+        if GENERATE_BBOX and SHOW_IMG:
+            bbox_im.show()
+
+    return all_imgs, all_labels
+
+
 def main():
-    device = device_selector(False)
+    device = device_selector(True)
 
     transform = transforms.Compose(
         [transforms.ToTensor()]
@@ -132,39 +198,43 @@ def main():
 
     # Get absolute path to `ExpPatch-Pics` directory
     abs_path = dirname(dirname(abspath(__file__)))
-    patch_path = join(abs_path, "ExpPatch-Pics")
+    patch_path = join(abs_path, "ExpPatch-Pics", "ExpPatchPics-Processed", "2015-08-28_Survey_24_P")
 
-    train_set = PatchPicsDataset(dataset_path=patch_path, transform=transform)
-    # test_set = PatchPicsDataset(dataset_path="wherever test data is")
+    full_dataset = PatchPicsDataset(dataset_path=patch_path, transform=transform)
 
-    trainloader = torch.utils.data.DataLoader(
-        train_set, batch_size=batch_size, shuffle=True, num_workers=0, collate_fn=windowed_collate_fn,
+    # Split dataset into train-test sets
+    # train_size = int(0.9 * len(full_dataset))
+    train_size = int(1 * len(full_dataset))
+    test_size = len(full_dataset) - train_size
+    train_set, test_set = torch.utils.data.random_split(full_dataset, [train_size, test_size])
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=batch_size, shuffle=False, num_workers=0, collate_fn=windowed_collate_fn,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_set, batch_size=batch_size, shuffle=False, num_workers=0, collate_fn=windowed_collate_fn,
     )
 
-    #t = windowed_collate_fn(train_set[0])
-    # print(type(train_set[0][0]), type(train_set[0][1]))
-    # print(f"shape trainset[0]: {train_set[0][0].shape}")
-    # print(f'train set[0]:\n{train_set[0][0]}\n-----------------------------------------')
-    # print(f'train set[1]:\n{train_set[0][1]}\n-----------------------------------------')
-
-
-    # testloader = torch.utils.data.DataLoader(
-    #     test_set, batch_size=batch_size, shuffle=True, num_workers=0
-    # )
-
     model = models.resnet50(pretrained=True)
+    for i, child in enumerate(model.children()):
+        if i < 5:
+            for param in child.parameters():
+                param.requires_grad = False
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=lr)
 
     trainer = Trainer(
-        data_loader=trainloader,
+        data_loader=train_loader,
+        test_data_loader=test_loader,
         model=model,
         optimizer=optimizer,
         loss_fn=loss_fn,
         device=device,
     )
 
-    trainer.fit_model(1)
+    trainer.fit(1)
+
+    trainer.eval()
 
 
 if __name__ == "__main__":
--- scripts/trainer/train.py
+++ scripts/trainer/train.py
@@ -1,13 +1,26 @@
 import torch
+import logging
 
 from os.path import abspath, dirname, join
 from torch.utils.data.dataloader import DataLoader as DataLoader
+
+from sklearn.metrics import f1_score, confusion_matrix
+
+
+logger = logging.getLogger("train.py")
+
+logging.basicConfig(
+    format="%(asctime)s [%(name)s: %(levelname)s] | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
 
 
 class Trainer:
     def __init__(
         self,
         data_loader: DataLoader,
+        test_data_loader: DataLoader,
         model: torch.nn.Module,
         optimizer,
         loss_fn,
@@ -15,9 +28,9 @@ class Trainer:
         output_dir=None,
         log_interval: int = 100,
         lr_scheduler=None,
-        callbacks=None,
     ):
         self._data_loader = data_loader
+        self._test_data_loader = test_data_loader
         self._model = model.to(device)
         self._device = device
         self._optimizer = optimizer
@@ -27,18 +40,6 @@ class Trainer:
         self._log_interval = log_interval
 
         self._global_step = 0
-
-        if callbacks is not None:
-            self._callbacks = callbacks.copy()
-
-            # Set Callback properties
-            for callback in self._callbacks:
-                callback.model = self._model
-                callback.optimizer = self._optimizer
-                callback.lr_scheduler = self._lr_scheduler
-                callback.batch_size = self._data_loader.batch_size
-        else:
-            self._callbacks = None
 
     def _check_property(self, prop_name: str):
         """Check that a property has been set.
@@ -57,7 +58,7 @@ class Trainer:
     def global_step(self):
         return self._global_step
 
-    def fit_model(
+    def fit(
         self,
         epochs: int,
     ):
@@ -68,41 +69,38 @@ class Trainer:
         """
         self._global_step = 0
         total_steps = epochs * len(self._data_loader)
-
+        self._model.train()
         # start_before()
+        print("Starting Training")
         for epoch in range(epochs):
             runnning_loss = 0.0
 
             for batch_step, batch in enumerate(self._data_loader):
-                X, y = batch[0].to(self._device), batch[1].to(self._device)
 
-                # Zero the parameter gradients
-                self._optimizer.zero_grad()
+                # for mini_batch in zip(torch.split(batch[0], 2), torch.split(batch[1], 2)):
+                for mini_batch in zip(batch[0], batch[1]):
+                    # for X, y in zip(mini_batch[0], mini_batch[1]):
+                    X, y = mini_batch
+                    X = X.to(self._device)
+                    y = y.type(torch.LongTensor).to(self._device)
+                    # Zero the parameter gradients
+                    self._optimizer.zero_grad()
 
-                # Forward + Backward + Optimize
-                outputs = self._model(X)
-                loss = self._loss_fn(outputs, y)
-                loss.backward()
-                self._optimizer.step()
+                    # Forward + Backward + Optimize
+                    outputs = self._model(X)
+                    loss = self._loss_fn(outputs, y)
+                    loss.backward()
+                    self._optimizer.step()
 
-                curr_loss = loss.item()
-                if self._callbacks is not None:
-                    _, predicted = torch.max(outputs.data, 1)
-                    for callback in self._callbacks:
-                        callback(
-                            global_step=self._global_step,
-                            total_steps=total_steps,
-                            X=X,
-                            y=y,
-                            loss=curr_loss,
-                            avg_loss=runnning_loss / (batch_step + 1),
-                            predicted=predicted,
-                        )
-                if self._lr_scheduler is not None:
-                    self._lr_scheduler.step()
+                    curr_loss = loss.item()
+                    if self._lr_scheduler is not None:
+                        self._lr_scheduler.step()
 
-                runnning_loss += curr_loss
-                self._global_step += 1
+                    runnning_loss += curr_loss
+                    if self._global_step % 100 == 0:
+                        print(f"Epoch: {self._global_step} | loss: {curr_loss}")
+
+                    self._global_step += 1
 
     def save_model(self):
         """Save the feature vector."""
@@ -111,3 +109,36 @@ class Trainer:
                 self._model(),
                 join(dirname(dirname(dirname(abspath(__file__)))), self._output_dir),
             )
+
+    @torch.no_grad()    # We don't need to calculate gradient
+    def eval(self):
+        total_samples = 0
+        correct = 0
+        self._model.eval()
+        y_true = []
+        y_pred = []
+
+        for global_step, batch in enumerate(self._test_data_loader):
+            for mini_batch in zip(batch[0], batch[1]):
+                X, y = mini_batch
+                X, y = X.to(self._device), y.to(self._device)
+                X = torch.as_tensor(X)
+                y = torch.as_tensor(y)
+
+                # calculate outputs by running images through the network
+                outputs = self._model(X)
+
+                _, predicted = torch.max(outputs.data, 1)
+                total_samples += y.size(0)
+                correct += (predicted == y).sum().item()
+                y_true.append(y)
+                y_pred.append(predicted)
+
+        logging.info(
+            "Accuracy of the network on the %d test images: %f",
+            total_samples,
+            (100 * correct // total_samples)
+        )
+
+        f1_score(y_true=y_true, y_pred=y_pred)
+
